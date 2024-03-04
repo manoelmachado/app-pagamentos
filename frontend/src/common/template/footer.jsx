318a06d49ab68542fbe0d833b6b428aadf53ded9
Manoel Machado <108824688+manoelmachado@users.noreply.github.com>
2024-03-04 11:33:51 -0300

    footer.jsx criado
    componente footer.jsx criado

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2024
+            <a href="https://www.linkedin.com/in/manoel-machado/" target="_blank">  Manoel Machado</a>
+        </strong>
+    </footer>
+)
